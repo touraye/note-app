5843617fda71f9f02c032d8330b032adefd40597
ebrima <61189190+touraye@users.noreply.github.com>
2022-12-28 04:10:30 -0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
 		<footer className='footer'>
 			<div className='foot1'>
-				Build by <a className='link' href='http://netlify.app/paebou.com'>pa ebou</a>
+				Build by <a className='link' href='https://ecodes.gm'>ecodes</a>
       </div>
       <div className="foot2">
         Copyright &copy; 2022
